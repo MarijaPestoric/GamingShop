5f3edc1d2c9be14e9a1e6989b8284e6c140989a4
Aleksandar Drašković <aco.draskovic2016@gmail.com>
2022-02-24 20:12:26 +0100

    about us page, card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom'
-import Home from './components/Home/Home';
-import ShoppingCart from './components/ShoppingCart/ShoppingCart';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer'
-import Dashboard from './components/Dashboard/Dashboard';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register'
-import Contact from './components/Contact/Contact';
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <Route exact path="/" component={Home}></Route>
-        <Route exact path='/shopping-cart' component={ShoppingCart}></Route>
-        <Route exact path='/dashboard' component={Dashboard}></Route>
-        <Route exact path='/login' component={Login}></Route>
-        <Route exact path='/register' component={Register}></Route>
-        <Route exact path='/contact' component={Contact}></Route>
+        <Route exact path="/shopping-cart" component={ShoppingCart}></Route>
+        <Route exact path="/dashboard" component={Dashboard}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+        <Route exact path="/about" component={About}></Route>
         <Footer />
       </div>
     </BrowserRouter>
---
+++ src/components/About/Card.js
@@ -0,0 +1,31 @@
+import "./Card.css";
+
+const Card = ({ firstName, lastName, photo, position, socials }) => {
+  console.log(firstName, photo);
+  return (
+    <div className="card">
+      <div className="card__bg"></div>
+      <img
+        src={`/images/team/${photo}`}
+        className="card__img"
+        alt={`${firstName} ${lastName} team member`}
+      />
+      <div className="card__content">
+        <h2 className="card__fullName">
+          {firstName} {lastName}
+        </h2>
+        <h3 className="card__position">{position}</h3>
+
+        <div className="card__socials">
+          {socials.map(({ name, url }) => (
+            <a className="social-link" href={url} target="_blank">
+              <img className="social-icon" src={`/images/icons/${name}.svg`}/>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
