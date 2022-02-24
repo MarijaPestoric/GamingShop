1710d43d9e92c13e56ccc6eeab9e7326d6a218b8
MarijaPestoric <60827834+MarijaPestoric@users.noreply.github.com>
2022-02-24 16:22:26 +0100

    for merge

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer'
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Checkout from './components/Checkout/Checkout';
+import Register from './components/Register/Register'
+import Contact from './components/Contact/Contact';
 function App() {
   return (
     <BrowserRouter>
@@ -18,7 +18,7 @@ function App() {
         <Route exact path='/dashboard' component={Dashboard}></Route>
         <Route exact path='/login' component={Login}></Route>
         <Route exact path='/register' component={Register}></Route>
-        <Route path='/checkout' component={Checkout}></Route>
+        <Route exact path='/contact' component={Contact}></Route>
         <Footer />
       </div>
     </BrowserRouter>
